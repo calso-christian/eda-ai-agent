89cc4380bee2fdad2488e224d1ffbb42edf7769f
calso-christian <calso.cpr@gmail.com>
2025-04-11 14:23:22 +0800

    Modified Agent to be a Module

---
+++ src/agent.py
@@ -0,0 +1,17 @@
+from langchain.chat_models import init_chat_model
+from langchain_community.agent_toolkits import create_sql_agent
+from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
+
+from dotenv import load_dotenv
+from load import load_data
+
+load_dotenv()
+
+def agent_start():
+    db=load_data(r'data\march_detalized.xlsx',"performance","detalized")
+    llm = init_chat_model('gpt-4o-mini', model_provider='openai')
+
+    toolkit = SQLDatabaseToolkit(db=db, llm=llm, top_k=None)
+    agent_executor = create_sql_agent(llm=llm, toolkit=toolkit, agent_type="openai-tools", verbose=True)
+    
+    return agent_executor
